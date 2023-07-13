e6c4467bfc4ccbac93149ec196cafa4c2831678c
byatham <balaiahy23@gmail.com>
2023-07-13 10:29:02 +0530

    class B modified

--- src/sd/com/oops/B.java
+++ src/sd/com/oops/B.java
@@ -20,7 +20,13 @@ public class B  extends A{
 		// TODO Auto-generated method stub
 		
 		B b=new B();
+		//B b1=new A();// we cannot assign parent class object child class reference. not possible
 		b.initialize("Balu", "Yellow");
+		System.out.println("***********************");
+		//creating parent class object
+		
+		A a=new A();
+		A a1=new B();// we can assign child class object parent class reference.
 	}
 
 }
