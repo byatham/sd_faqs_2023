42dda6b097348ad361c5c348f86b069d7117e765
byatham <balaiahy23@gmail.com>
2023-07-13 10:47:04 +0530

    class PillaofOOPSadded

---
+++ src/sd/com/oops/PillarOfOOPS.java
@@ -0,0 +1,32 @@
+package sd.com.oops;
+
+public class PillarOfOOPS {
+	//1. Encapslation
+	
+	//define attributes as private
+	// define public methods with setters and getters
+	
+	
+	//2. Polymorphism
+	// by using overloading and overriding we can achieve Polymorphism
+	// *** over loading***
+	//in same class possible
+	// by changing order of parameters and types of parameter we can achieve
+	// calling as compile time 
+	
+	// *** overriding ***
+	// possible in two different classes with inheritance relation
+	// method name and signature should be same
+	// calling as runtime polymorphism
+	
+	//3.Inheritance
+	
+	//in inheritance we will use re usability purpose
+	// we can achieve among classes by using extends 
+	// with class and interface by using implements
+	
+	
+	//Abstraction
+// hiding the implementation details and showing functionality the user is called abstraction
+	//by using interfaces and abstract class
+}
