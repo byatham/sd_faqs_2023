0b065b4b16dee192d4361865b631cb71501a24d2
byatham <balaiahy23@gmail.com>
2023-07-14 10:55:51 +0530

    modifed class

--- src/sd/com/oops/Student.java
+++ src/sd/com/oops/Student.java
@@ -1,7 +1,29 @@
 package sd.com.oops;
 
 public class Student {
+	public Student(String name, int id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
 	private String name;
+	private int id;
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		Student st=(Student)obj;
+		return this.id==st.getId() && this.name.equals(st.getName());
+		
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		return id;
+	}
+	
+	
 	public String getName() {
 		return name;
 	}
@@ -14,6 +36,15 @@ public class Student {
 	public void setId(int id) {
 		this.id = id;
 	}
-	private int id;
+	public static void main(String[] args) {
+		Student st=new Student("Balu",101);
+		Student st1=new Student("Raju",101);
+		System.out.println("hashCode() same :: "+(st.hashCode()==st1.hashCode()));
+		
+		System.out.println("is equals() same :: "+st.equals(st1));
+		
+		System.out.println("hello "+st.getName());
+
+	}
 
 }
